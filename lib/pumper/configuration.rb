48f3c2b42469a2e7ccfa372ccf375eb802ca79c4
ssnikolay <ssnikolay@gmail.com>
2015-01-14 18:56:20 +0400

    parse config option

--- lib/pumper/configuration.rb
+++ lib/pumper/configuration.rb
@@ -1,6 +1,7 @@
 module Pumper
   class Configuration
     class ProjectNotSet < StandardError; end
+    class InvalidOptions < StandardError; end
 
     class << self
       def configure!(options)
@@ -12,7 +13,8 @@ module Pumper
       private
 
       def validate(options)
-        raise ProjectNotSet if options[:project].nil?
+        raise ProjectNotSet.new('You need set project (--project <PATH_TO_PROJECT>)') if options[:project].nil?
+        raise InvalidOptions.new('Invalid options') if options[:config] && (options[:gemset] || options[:vendor])
       end
     end
   end
--- spec/lib/pumper/configuration_spec.rb
+++ spec/lib/pumper/configuration_spec.rb
@@ -5,11 +5,18 @@ describe Pumper::Configuration do
 
   context 'when raise error ProjectNotSet' do
     it { expect { subject }.to raise_error(Pumper::Configuration::ProjectNotSet) }
+    it { expect { subject }.to raise_exception('You need set project (--project <PATH_TO_PROJECT>)') }
   end
 
   context 'when valid project' do
     let(:options) { { project: 'cashier' } }
 
     it { is_expected.to eq(options) }
+  end
+
+  context 'when raise error InvalidOptions' do
+    let(:options) { { project: 'cashier', config: true, gemset: 'ruby-2.1.0' } }
+
+    it { expect { subject }.to raise_error(Pumper::Configuration::InvalidOptions) }
   end
 end
